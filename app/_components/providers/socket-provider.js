4d6672e99f2527b14a808f7c7df77e796f052fac
jaekim <jaekim@fadutec.com>
2024-11-11 13:56:29 +0900

    fix: change socket IP address using env

--- app/_components/providers/socket-provider.js
+++ app/_components/providers/socket-provider.js
@@ -1,46 +1,42 @@
-'use client'
+"use client";
 
-import {
-    createContext,
-    useContext,
-    useEffect,
-    useState
-} from 'react';
-import { io as IoClient } from 'socket.io-client';
+import { createContext, useContext, useEffect, useState } from "react";
+import { io as IoClient } from "socket.io-client";
 
 const SocketContext = createContext({
-    socket: null,
-    connected: false
+  socket: null,
+  connected: false,
 });
 
 export const useSocket = () => useContext(SocketContext);
 
 export const SocketProvider = ({ children }) => {
-    const [socket, setSocket] = useState(null);
-    const [connected, setConnected] = useState(false);
+  const [socket, setSocket] = useState(null);
+  const [connected, setConnected] = useState(false);
 
-    useEffect(() => {
-        const socket = IoClient(`http://10.40.10.84:8200`);
-        // const socket = IoClient(`http://${window.location.hostname}:8200`);
-        socket.on('connect', () => {
-            setConnected(true);
-        });
-        socket.on('disconnect', () => {
-            setConnected(false);
-        });
-        setSocket(socket);
+  useEffect(() => {
+    const socket = IoClient(`${process.env.NEXT_PUBLIC_SOCKET_URL}`);
+    socket.on("connect", () => {
+      setConnected(true);
+    });
+    socket.on("disconnect", () => {
+      setConnected(false);
+    });
+    setSocket(socket);
 
-        return () => {
-            socket.disconnect();
-        }
-    }, []);
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
 
-    return (
-        <SocketContext.Provider value={{
-            socket,
-            connected
-        }}>
-            {children}
-        </SocketContext.Provider>
-    )
-}
+  return (
+    <SocketContext.Provider
+      value={{
+        socket,
+        connected,
+      }}
+    >
+      {children}
+    </SocketContext.Provider>
+  );
+};
